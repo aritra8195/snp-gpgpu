08803f20cb0db5dc1321289ad430479c1dabca2a
jounin.f <jounin.f@c3fad153-d5cd-e596-5e82-f63cdd42d974>
2010-12-08 18:02:28 +0000

    - added working versions from ACL iMac

--- f/test_code/scanf.cu
+++ f/test_code/scanf.cu
@@ -1,26 +1,31 @@
 #include <stdio.h>
 
 int main ( void ) {
-	int x;
+	int x, y;
+	char *str;
 	while( x != 3) {
-	printf( "\n Type \n 1 to enter filename \n 2 for 2 \n 3 to quit \n: " );
-	scanf( "%d", &x );
-	switch( x ){
-		case 1:
-			char *str;
-			printf( " Please enter filename: \n" );
-			scanf( "%s", str );
-			printf( " You entered the filename %s ", str );
-			break;
-                case 2: 
-                        printf( " You entered 2 \n" );
-                        break;
-                case 3: 
-                        printf( " You entered quit. Bye! \n" );
-                        break;
-		default:
-			printf( " You entered an invalid choice \n" );
-			break;
-	}
+		printf( "\n Type \n 1 to enter filename \n 2 for 2 \n 3 to quit \n: " );
+		scanf( "%d", &x );
+		switch( x ){
+			case 1:
+				y = 0;
+				printf( " Please enter filename: \n" );
+				scanf(" %c",&str);
+				while( str[ y ] != EOF ){ 
+					scanf(" %c",&str);
+				}
+				printf( " You entered the filename %s ", str);
+				break;
+			case 2: 
+				printf( " You entered 2 \n" );
+				break;
+			case 3: 
+				printf( " You entered quit. Bye! \n" );
+				break;
+			default:
+				printf( " You entered an invalid choice \n" );
+				break;
+		}
+
 	}
 }
