dbc51400e82d1dcf5ed08f30a41541cf60dbc1cc
jounin.f <jounin.f@c3fad153-d5cd-e596-5e82-f63cdd42d974>
2010-12-18 11:25:49 +0000

    - a 'better' pseudo-random integer generator, using the user input seed value + current time in seconds

--- trunk/f/test_code/random.cu
+++ trunk/f/test_code/random.cu
@@ -1,83 +1,44 @@
 /*
-Steven Billington
-January 17, 2003
-exDice.cpp
-Program rolls two dice with random
-results.
-*/
-/*
-Header: iostream
-Reason: Input/Output stream
-Header: stdlib
-Reason: For functions rand and srand
-Header: time.h
-Reason: For function time, and for data type time_t
-*/
-#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
 #include <time.h>
-/*
-These constants define our upper
-and our lower bounds. The random numbers
-will always be between 1 and 6, inclusive.
-*/
+
+//upper and lower bounds, inclusive
 const int LOW = 1;
-const int HIGH = 100;
+const int HIGH = 6;
 int main()
 {
-/*
-Variables to hold random values
-for the first and the second die on
-each roll.
-*/
 int first_die, sec_die;
-/*
-Declare variable to hold seconds on clock.
-*/
+
+//Declare variable to hold seconds on clock.
+
 time_t seconds;
-/*
-Get value from system clock and
-place in seconds variable.
-*/
+
+//Get value from system clock and place in seconds variable.
+
 time(&seconds);
-/*
-Convert seconds to a unsigned
-integer.
-*/
+
+// Convert seconds to a unsigned int
+
 srand((unsigned int) seconds);
-/*
-Get first and second random numbers.
-*/
 first_die = rand() % (HIGH - LOW + 1) + LOW;
 sec_die = rand() % (HIGH - LOW + 1) + LOW;
-/*
-Output first roll results.
-*/
 printf( "Your roll is %d, %d \n", first_die, sec_die );
-//cout<< "Your roll is (" << first_die << ", "
-//<< sec_die << "}" << endl << endl;
-/*
-Get two new random values.
-*/
-first_die = rand() % (HIGH - LOW + 1) + LOW;
-sec_die = rand() % (HIGH - LOW + 1) + LOW;
-/*
-Output second roll results.
-*/
-printf( "My roll is %d, %d \n", first_die, sec_die );
-//cout<< "My roll is (" << first_die << ", "
-//<< sec_die << "}" << endl << endl;
 return 0;
 }
-
-/*#include <stdlib.h>
+*/
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h> 
  
-int randomize( int maxIntVal ){
+int randomize( int maxIntVal, int seed1 ){
 //	int x;
 	time_t seconds;
 	time( &seconds );
+	seconds += seconds + seed1;
 	srand( ( unsigned int ) seconds );
+//	srand( ( unsigned ) time( NULL ) );
+//	srand( ( unsigned ) seed );
 //	int max = atoi( argv[ 1 ] );
 	return rand( ) % maxIntVal + 1;	
 }
@@ -88,28 +49,28 @@ int main( int argc, char *argv[ ] )
 	if ( argc != 2 )
 		printf( "\nUsage:\n %s maxrandomvalue\n", argv[ 0 ] );
 	else {	
-		int max = atoi( argv[ 1 ] );
+		int randMax = atoi( argv[ 1 ] );
 		int x, randNum;
 		for ( x = 0; x < 50; x++ ){
-			randNum = randomize( max );
+			randNum = randomize( randMax, x * randMax );
 			printf( "%i ", randNum );
 		}
 	}
 }
-*/
+
 /*
-# random numbers in a range
+// random numbers in a range
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h> 
  
-void main()
+int main( void )
 {
 int x ;
 srand((unsigned)time(NULL));
 
 for(x=0;x<=100;x++)
-printf("%i\t",rand()%10 + 1);
+printf("%i\t",rand()%99 + 1);
 
 }
 */
