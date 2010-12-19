1851400c69ca8f1f1eaf049c33c490032509fd3f
jounin.f <jounin.f@c3fad153-d5cd-e596-5e82-f63cdd42d974>
2010-12-19 15:57:49 +0000

    added code to load and print/determine rules from neurons

---
+++ f/test_code/loadRules.cu
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main ( int argc, char *argv[ ] ){
+	if ( argc != 2 )
+		printf( "\n Usage \n%s filetoread \n", argv[ 0 ] );
+	else {
+		char x; 
+		int rules[ 50 ]; //length of rules that can be read
+		int a = 0;
+		char *filename = argv[ 1 ];
+		FILE *ptr1 = fopen( filename, "r" );
+		fscanf( ptr1, "%c", &x  );
+		while( !feof( ptr1 ) ) {
+			if ( isalnum( x ) ) {
+				int y = atoi( &x );
+				//printf( "\n%d", y );
+				fscanf( ptr1, "%c", &x );
+				rules[ a ] = y;
+			}
+			else { // ! = 33, $ = 36, ' ' = 32
+				//printf( "-ELSE-" );
+				rules[ a ] = -1;
+				fscanf( ptr1, "%c", &x );
+			}
+			a++;
+		}
+	//print the loaded rules
+	// Rules on file: 2 2 $ 1 $ 1 2
+	// Rules on load: |2 |-1 |2 |-1 |-1 |-1 |1 |-1 |-1 |-1 |1 |-1 |2 |-1 |
+		printf( "\n" );
+		int oneCnt = 1;
+		int neuron = 1;
+		printf( "Neuron %d rule/s:\n", neuron );
+		for( int x = 0; rules[ x ] != 0; x++) {
+			//printf( "%d |", rules[ x ] );
+			if ( rules[ x ] > 0 && oneCnt < 4 ){
+				printf( " %d ", rules[ x ] );
+				oneCnt = 1;
+//				printf( " A " );
+			}
+			else if ( rules[ x ] < 0 && oneCnt < 3 ) {
+				oneCnt = oneCnt + 1;
+//				printf( " B " );
+			}
+			else if ( rules[ x ] < 0 && oneCnt == 3 ) {
+				neuron = neuron + 1;
+				printf( "\nNeuron %d rule/s:\n", neuron );
+				oneCnt = 1;
+//				printf( " C " );
+			}
+		} 
+	//	printf( " '%c' '%d' '%c' '%d' '%c' '%d'", " ", " ", "$", "$", 33, 33 );
+		printf( "\n" );
+	}	
+}
--- f/test_code/random.cu
+++ f/test_code/random.cu
@@ -1,32 +1,3 @@
-/*
-#include <iostream>
-#include <cstdlib>
-#include <time.h>
-
-//upper and lower bounds, inclusive
-const int LOW = 1;
-const int HIGH = 6;
-int main()
-{
-int first_die, sec_die;
-
-//Declare variable to hold seconds on clock.
-
-time_t seconds;
-
-//Get value from system clock and place in seconds variable.
-
-time(&seconds);
-
-// Convert seconds to a unsigned int
-
-srand((unsigned int) seconds);
-first_die = rand() % (HIGH - LOW + 1) + LOW;
-sec_die = rand() % (HIGH - LOW + 1) + LOW;
-printf( "Your roll is %d, %d \n", first_die, sec_die );
-return 0;
-}
-*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h> 
@@ -57,20 +28,3 @@ int main( int argc, char *argv[ ] )
 		}
 	}
 }
-
-/*
-// random numbers in a range
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h> 
- 
-int main( void )
-{
-int x ;
-srand((unsigned)time(NULL));
-
-for(x=0;x<=100;x++)
-printf("%i\t",rand()%99 + 1);
-
-}
-*/
