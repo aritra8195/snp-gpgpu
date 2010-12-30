84f6a2ddda9855c7135edf5ee766b68b2dc12707
jounin.f <jounin.f@c3fad153-d5cd-e596-5e82-f63cdd42d974>
2010-12-30 18:11:35 +0000

    - updated snp.py to handle rules loading

--- f/test_code/snp.py
+++ f/test_code/snp.py
@@ -1,18 +1,29 @@
-#import os
-
-#load confVec c0 (Ck+1 several times), spikVec s0 (Sk+1 several times), spikTransMat M (once), and rules r (once)
-
-#fConfVec = open( 
 import sys
 
+#TODOs:
+#load confVec c0 (Ck+1 several times), spikVec s0 (Sk+1 several times), spikTransMat M (once), and rules r (once)
+#fConfVec = open( 
+
+###
+#START of aux functions
+###
+
+#START of function to import vectors/matrices from file/s
 def importVec( filename ) :
 	filePtr = open( filename, 'rb' )
-	return filePtr.read( )
-	
+	Vec = filePtr.read( )
+	return Vec.split( )
+#END of function to import vectors/matrices from file/s	
 
-#print sys.argv[ 3 ]
-#for arg in sys.argv: 
-#    print arg
+###
+#END of aux functions
+###
+
+
+
+###
+#START of Main Program Flow
+###
 if ( len( sys.argv ) < 5 ) :
 	print '\n Program usage:\n'+sys.argv[ 0 ] + ' confVec spikVec spikTransMat rules\n'
 
@@ -21,5 +32,18 @@ else :
 	spikVec = importVec( sys.argv[ 2 ] )
 	spikTransMat  = importVec( sys.argv[ 3 ] )
 	rules = importVec( sys.argv[ 4 ] )
-	print ' \nconfVec:\n' + confVec +'\nspikVec:\n'+spikVec+'\nspikTransMat:\n'+spikTransMat+'\nrules:\n'+rules #works
-	
+	#print ' \nconfVec:\n' + confVec +'\nspikVec:\n'+spikVec+'\nspikTransMat:\n'+spikTransMat+'\nrules:\n'+rules #works
+	#for elem in confVec[ 2: ] : # the 2: is so that the loop starts @ index 2
+	#x = 1	
+	#print 'Neuron %d' % ( x )	
+	#for rule in rules : #for loop works
+	#	if rule == '$' :
+	#		x += 1
+	#		print ' Neuron %d' % ( x )
+	#	else :
+	#		print rule #works
+
+###
+#END of Main Program Flow
+###
+
