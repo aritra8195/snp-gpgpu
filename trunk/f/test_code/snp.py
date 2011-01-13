3cf1e19c01d0d82cbc668e99728650c54c84fd11
jounin.f <jounin.f@c3fad153-d5cd-e596-5e82-f63cdd42d974>
2011-01-13 15:46:28 +0000

    - made the function genNeurSpikVecStr( ) which just implements the code done previously in a functional manner :)

--- trunk/f/test_code/snp.py
+++ trunk/f/test_code/snp.py
@@ -3,6 +3,11 @@ import sys
 #TODOs:
 #load confVec c0 (Ck+1 several times), spikVec s0 (Program must determine this!),
 # spikTransMat M (once), and rules r (once)
+#create function to implement the spike-rule selection (SRS) criterion
+#QUESTION: how to implement
+# 1) a( aa )+ ( a bit more elaborate reg ex)
+# 2) a^2/a -> a	(reg ex not equal to spikes consumed)
+# 3) a^2 -> a ???
 
 ###
 #START of AUX functions
@@ -117,8 +122,56 @@ def genPotentialSpikrule( spikRuleList ) :
 
 
 #START of function
-#def genSpikVecStr( tmpList, idx ) :
-	
+def genNeurSpikVecStr( tmpList, neurNum ) :
+	#generate all possible + valid 10 strings per neuron	
+	x = 0
+	tmp3 = [ ]
+	# loop over total number of neurons ( x )
+	while x < neurNum :
+		#print ' x = ', x #works - 
+		y = 1
+		#get the max number of elements of tmpList that satisfy the SRS criterion
+		tmp4 = [ ]
+		# loop over total number of rules ( y )
+		while y <= max( tmpList[ x ][ 1: ] ) :
+			#print '\ty = ', y			
+			# to get index of a certain value val in list, use index() e.g. x = list.index( val )
+			# replace with 1, the value of tmpList whose index is equal to y, 0 on ever other element
+			# find which index has value == x
+			# idx = tmpList.index( y )
+			#genSpikVecStr( tmpList, y )
+			z = 1
+			spikStr = ''
+			while z <= len( tmpList[ x ][ 1: ] ) :
+				#print '\t\tz =', z #, range( len( tmpList[ x ] ) )
+				#print '\t\t tmpList[ x ][ z ] =', int( tmpList[ x ][ z ] )
+				if int( tmpList[ x ][ z ] ) == y :
+#					print 'IF', int( tmpList[ x ][ z ] )
+					spikStr = spikStr + '1'
+				else :
+#					print 'ELSE', int( tmpList[ x ][ z ] )
+					spikStr = spikStr + '0'
+				z += 1
+			#print '\t\t', spikStr
+			tmp4[ y - 1: ] = [ spikStr ]
+			#print '\t\t', tmp4
+
+			#create function to implement the spike-rule selection (SRS) criterion
+			#QUESTION: how to implement
+			# 1) a( aa )+ ( a bit more elaborate reg ex)
+			# 2) a^2/a -> a	(reg ex not equal to spikes consumed)
+			# 3) a^2 -> a ???
+
+			#chkSRS (check SRS). Currently only type 3) are implemented here
+
+			# builds nested list: tmp = [ '01', '10 ] tmp2 = [ '1' ] then tmp = [ tmp, tmp2 ] to get tmp = [ [ '01', '10 ], [ '1' ] ]	
+
+			#print '\tB' #works
+			y += 1 	
+		tmp3[ x: ] = [ tmp4 ]	
+		x += 1
+		#print tmp3
+	return tmp3
 #END of function
 
 ###
@@ -162,60 +215,14 @@ else :
 	#output should be : [['2', 1, 2], ['1', 1], ['1', 1, 0]]  
 
 	tmpList = genPotentialSpikrule( spikRuleList )
-	print ' tmpList = ', tmpList
+	#print ' tmpList = ', tmpList
 
 	# get min/max values in a list: min( list) and max( list )
 	
-	#generate all possible + valid 10 strings per neuron	
-	x = 0
-	tmp3 = [ ]
-	# loop over total number of neurons ( x )
-	while x < neurNum :
-		print ' x = ', x #works - 
-		y = 1
-		#get the max number of elements of tmpList that satisfy the SRS criterion
-		tmp4 = [ ]
-		# loop over total number of rules ( y )
-		while y <= max( tmpList[ x ][ 1: ] ) :
-			print '\ty = ', y			
-			# to get index of a certain value val in list, use index() e.g. x = list.index( val )
-			# replace with 1, the value of tmpList whose index is equal to y, 0 on ever other element
-			# find which index has value == x
-			# idx = tmpList.index( y )
-			#genSpikVecStr( tmpList, y )
-			z = 1
-			spikStr = ''
-			while z <= len( tmpList[ x ][ 1: ] ) :
-				#print '\t\tz =', z #, range( len( tmpList[ x ] ) )
-				#print '\t\t tmpList[ x ][ z ] =', int( tmpList[ x ][ z ] )
-				if int( tmpList[ x ][ z ] ) == y :
-#					print 'IF', int( tmpList[ x ][ z ] )
-					spikStr = spikStr + '1'
-				else :
-#					print 'ELSE', int( tmpList[ x ][ z ] )
-					spikStr = spikStr + '0'
-				z += 1
-			#print '\t\t', spikStr
-			tmp4[ y - 1: ] = [ spikStr ]
-			print '\t\t', tmp4
+	# generate all possible + valid 10 strings PER neuron
+	tmpList = genNeurSpikVecStr( tmpList, neurNum )
 
-			#create function to implement the spike-rule selection (SRS) criterion
-			#QUESTION: how to implement
-			# 1) a( aa )+ ( a bit more elaborate reg ex)
-			# 2) a^2/a -> a	(reg ex not equal to spikes consumed)
-			# 3) a^2 -> a ???
-
-			#chkSRS (check SRS). Currently only type 3) are implemented here
-
-			# do something like tmp = [ '01', '10 ] tmp2 = [ '1' ] then tmp = [ tmp, tmp2 ] to get tmp = [ [ '01', '10 ], [ '1' ] ]
-			
-
-			#print '\tB' #works
-			y += 1 	
-		tmp3[ x: ] = [ tmp4 ]	
-		x += 1
-		print tmp3
-	
+	print ' tmpList = ', tmpList
 ###
 #END of MAIN Program Flow
 ###
